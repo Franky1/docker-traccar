b5fefd50e68ba3d8bc8ef1d9d0b70ed6c35df91e
Franky1 <Franky1@users.noreply.github.com>
2017-08-22 23:35:33 +0200

    Create docker-run.sh

---
+++ docker-run.sh
@@ -0,0 +1,11 @@
+docker run \
+-d --restart always \
+--name traccar \
+--hostname traccar \
+-p 8082:8082 \
+-p 5000-5150:5000-5150 \
+-p 5000-5150:5000-5150/udp \
+-v /etc/timezone:/etc/timezone:ro \
+-v /etc/localtime:/etc/localtime:ro \
+-v /var/docker/traccar/logs:/opt/traccar/logs:rw \
+-v /var/docker/traccar/traccar.xml:/opt/traccar/conf/traccar.xml:ro
