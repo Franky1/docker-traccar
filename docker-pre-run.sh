ccfaa4cd1998b68194481f6d2078809e245ac276
Franky1 <Franky1@users.noreply.github.com>
2017-08-22 23:36:01 +0200

    Update docker-pre-run.sh

--- docker-pre-run.sh
+++ docker-pre-run.sh
@@ -1,1 +1,3 @@
-docker run --rm --entrypoint cat /opt/traccar/conf/traccar.xml > /var/docker/traccar/traccar.xml
+docker run \
+--rm \
+--entrypoint cat /opt/traccar/conf/traccar.xml > /var/docker/traccar/traccar.xml
